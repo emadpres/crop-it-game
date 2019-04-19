d6061bcc8acb46dcc48d9f792da54cacffc69d70
Vahid <v.heidaripour@gmail.com>
2019-04-19 12:41:04 +0200

    GameOptions

---
+++ Classes/GameOptions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vahid Heidaripour on 2019-04-19.
+//
+
+#include "GameOptions.h"
---
+++ Classes/GameOptions.h
@@ -0,0 +1,14 @@
+//
+// Created by Vahid Heidaripour on 2019-04-19.
+//
+
+#ifndef CROPITGAME_GAMEOPTIONS_H
+#define CROPITGAME_GAMEOPTIONS_H
+
+
+class GameOptions {
+
+};
+
+
+#endif //CROPITGAME_GAMEOPTIONS_H
