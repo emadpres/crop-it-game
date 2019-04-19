3d4ca8e4b365ebab72780e7a96fc29b1fbaf0dcd
meslahik <eslahm@usi.ch>
2019-04-19 15:48:44 +0200

    simple credit page 2

--- Classes/Credit.h
+++ Classes/Credit.h
@@ -1,8 +1,14 @@
 //
 // Created by Mojtaba Eslahi Kelorazi on 2019-04-19.
 //
+#include "cocos2d.h"
 
-#ifndef CROPITGAME_CREDIT_H
-#define CROPITGAME_CREDIT_H
-
-#endif //CROPITGAME_CREDIT_H
+#ifndef __CREDIT_H__
+#define __CREDIT_H__
+class Credit: public cocos2d::LayerColor
+{
+public:
+    virtual bool init();
+    CREATE_FUNC(Credit);
+};
+#endif //__CREDIT_H__
