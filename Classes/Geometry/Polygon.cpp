4d256b4e02e69524a5ff574e206c406434530dab
Emad Aghajani <emadpres@gmail.com>
2019-04-20 15:37:22 +0200

    Angle Cropping

--- Classes/Geometry/Polygon.cpp
+++ Classes/Geometry/Polygon.cpp
@@ -7,11 +7,11 @@ using namespace std;
 Polygon::Polygon() {
 }
 
-void Polygon::AddSegment(pair<Vec2, Vec2> seg) {
+void Polygon::AddSegment(seg_t seg) {
     _segments.push_back(seg);
 }
 
-const list<pair<Vec2, Vec2>> &Polygon::GetSegments() const {
+const segList_t &Polygon::GetSegments() const {
     return _segments;
 }
 
@@ -35,8 +35,23 @@ void Polygon::Initial_Custom1(Vec2 leftBottom, int width) {
     AddSegment(make_pair(center + Vec2(-width / 2, 0), center + Vec2(-width / 2, +width / 2)));
 }
 
-segIterator Polygon::Ray(Vec2 startPos, Vec2 dir) {
-    segIterator nearestSeg_it = _segments.end();
+int Polygon::RayCount(Vec2 startPos, Vec2 dir) const {
+    int count = 0;
+    float s, t;
+    auto it = _segments.begin();
+    while (it != _segments.end()) {
+        if (Vec2::isLineIntersect(startPos, startPos + dir * 1000, (*it).first, (*it).second, &s, &t)) {
+            if (s >= 0 && t >= 0 && s <= 1 && t <= 1) {
+                count++;
+            }
+        }
+        it++;
+    }
+    return count;
+}
+
+segListIterator_t Polygon::Ray(Vec2 startPos, Vec2 dir) {
+    segListIterator_t nearestSeg_it = _segments.end();
     float nearestSeg_s = 1.1f;
     auto it = _segments.begin();
     float s, t;
@@ -55,24 +70,20 @@ segIterator Polygon::Ray(Vec2 startPos, Vec2 dir) {
     return nearestSeg_it;
 }
 
-void Polygon::BreakSegment(segIterator it, float breakRatio) {
-//    list<pair<Vec2, Vec2>>::iterator it = _segments.begin();
-//    int cur = 0;
-//    while(cur!=segIndex && it!= _segments.end()) {
-//        it++;
-//        cur++;
-//    }
+Vec2 Polygon::BreakSegment(segListIterator_t it, float breakRatio) {
     Vec2 start = (*it).first;
     Vec2 end = (*it).second;
     it->second.x = start.x + (end.x - start.x) * breakRatio;
     it->second.y = start.y + (end.y - start.y) * breakRatio;
-    auto updatedEnd = (*it).second;
+    auto firstPartEnd = (*it).second;
     it++;
-    _segments.insert(it, make_pair(updatedEnd, end));
+    _segments.insert(it, make_pair(firstPartEnd, end));
     CCLOG("New number of segments: %zu", _segments.size());
+
+    return firstPartEnd;
 }
 
-float Polygon::CalcCutPoint(Vec2 a_start, Vec2 a_end, Vec2 b_start, Vec2 b_end) {
+float Polygon::CalcCutPoint(Vec2 a_start, Vec2 a_end, Vec2 b_start, Vec2 b_end) const {
     float s, t;
     if (Vec2::isLineIntersect(a_start, a_end, b_start, b_end, &s, &t)) {
         if (s >= 0 && t >= 0 && s <= 1 && t <= 1) {
@@ -82,53 +93,106 @@ float Polygon::CalcCutPoint(Vec2 a_start, Vec2 a_end, Vec2 b_start, Vec2 b_end) 
     return 0;
 }
 
-void Polygon::Crop(Vec2 pos, int dir) {
-    if (dir == 1) // Right,Down
-    {
-        auto it = Ray(pos, Vec2(1, 0));
-        float r = CalcCutPoint(pos, pos + Vec2(1, 0) * 1000, it->first, it->second);
-        BreakSegment(it, r);
-    }
-//    int first = Crop(pos, pos+Vec2(1000,0));
-//    int second = Crop(pos, pos+Vec2(0,-1000));
-//    if(second<first) {
-//        int t = first;
-//        first = second;
-//        second = t+1;
-//    }
+void Polygon::Crop(Vec2 pos, int dir, Vec2 ballPos) {
 
-//    std::list<std::pair<cocos2d::Vec2, cocos2d::Vec2>> _newSegments;
-//    bool keepInnerPart = true;
-//    int i =0;
-//    for (const auto &seg: _segments)
-//    {
-//        if(keepInnerPart) {
-//            if(i>first && i<=second)
-//                _newSegments.push_back(seg);
-//
-//        }
-//        i++;
-//    }
-//
-//    _segments.clear();
-//    for (const auto &seg: _newSegments)
-//        _segments.push_back(seg);
+    if(false==IsPointInsidePolygon(pos))
+        return;
+
+    Vec2 dir1, dir2;
+    switch(dir)
+    {
+        case 1: // Right,Down
+            dir1 = Vec2(1, 0);
+            dir2 = Vec2(0, -1);
+            break;
+        case 2:
+            dir1 = Vec2(0, -1);
+            dir2 = Vec2(-1, 0);
+            break;
+        case 3:
+            dir1 = Vec2(-1, 0);
+            dir2 = Vec2(0, 1);
+            break;
+        case 4:
+            dir1 = Vec2(0, 1);
+            dir2 = Vec2(1, 0);
+            break;
+    }
+
+
+    segListIterator_t it1, it2;
+    Vec2 rayCollisionPoint1, rayCollisionPoint2;
+
+
+    it1 = Ray(pos, dir1);
+    float r = CalcCutPoint(pos, pos + dir1 * 1000, it1->first, it1->second);
+    rayCollisionPoint1 = BreakSegment(it1, r);
+
+    it2 = Ray(pos, dir2);
+    r = CalcCutPoint(pos, pos + dir2 * 1000, it2->first, it2->second);
+    rayCollisionPoint2 = BreakSegment(it2, r);
+
+    Polygon *poly1;
+    Polygon *poly2;
+
+    SplitPolygon(pos, it1, it2, rayCollisionPoint1, rayCollisionPoint2, poly1, poly2);
+
+    if (poly1->IsPointInsidePolygon(ballPos))
+        *this = *poly1;
+    else
+        *this = *poly2;
 }
 
-int Polygon::Crop(Vec2 start, Vec2 end) {
-    float s, t;
-    int breakIndex = 0;
-    auto it = _segments.begin();
-    while (it != _segments.end()) {
-        if (Vec2::isLineIntersect(start, end, (*it).first, (*it).second, &s, &t)) {
-            if (s >= 0 && t >= 0 && s <= 1 && t <= 1) {
-                BreakSegment(it, t);
-                break;
-            }
+void
+Polygon::SplitPolygon(Vec2 &pos, const segListIterator_t &it1, const segListIterator_t &it2, Vec2 &rayCollisionPoint1,
+                      Vec2 &rayCollisionPoint2, Polygon *&poly1, Polygon *&poly2) const {
+    poly1 = new Polygon();
+    poly2 = new Polygon();
+    bool reachedFirstBreakingPoint = false;
+    Polygon **activePoly = &poly1;
 
-        }
-        it++;
-        breakIndex++;
+    auto currentPolyIt = _segments.begin();
+    while (currentPolyIt != _segments.end()) {
+        if (currentPolyIt == it1 || currentPolyIt == it2) {
+            if (false == reachedFirstBreakingPoint) {
+                reachedFirstBreakingPoint = true;
+                (*activePoly)->AddSegment(make_pair(currentPolyIt->first, currentPolyIt->second));
+                if (currentPolyIt == it1) {
+                    (*activePoly)->AddSegment(make_pair(rayCollisionPoint1, pos));
+                    (*activePoly)->AddSegment(make_pair(pos, rayCollisionPoint2));
+                } else {
+                    (*activePoly)->AddSegment(make_pair(rayCollisionPoint2, pos));
+                    (*activePoly)->AddSegment(make_pair(pos, rayCollisionPoint1));
+                }
+                activePoly = (activePoly == &poly1) ? (&poly2) : (&poly1);
+                if (currentPolyIt == it1) {
+                    (*activePoly)->AddSegment(make_pair(rayCollisionPoint2, pos));
+                    (*activePoly)->AddSegment(make_pair(pos, rayCollisionPoint1));
+                } else {
+                    (*activePoly)->AddSegment(make_pair(rayCollisionPoint1, pos));
+                    (*activePoly)->AddSegment(make_pair(pos, rayCollisionPoint2));
+                }
+            } else {
+                (*activePoly)->AddSegment(make_pair(currentPolyIt->first, currentPolyIt->second));
+                activePoly = (activePoly == &poly1) ? (&poly2) : (&poly1);
+            }
+        } else
+            (*activePoly)->AddSegment(make_pair(currentPolyIt->first, currentPolyIt->second));
+        currentPolyIt++;
+
     }
-    return breakIndex;
+}
+
+bool Polygon::IsPointInsidePolygon(Vec2 point) const {
+    int c = RayCount(point, Vec2(1, 0));
+    return (c % 2 == 1);
+}
+
+
+Polygon &Polygon::operator=(const Polygon &second) {
+    _segments.clear();
+    for (const auto &seg: second._segments)
+        AddSegment(seg);
+
+    return (*this);
 }
--- Classes/Geometry/Polygon.h
+++ Classes/Geometry/Polygon.h
@@ -4,33 +4,43 @@
 #include <list>
 #include "cocos2d.h"
 
-using segIterator = std::list<std::pair<cocos2d::Vec2, cocos2d::Vec2>>::iterator;
+using seg_t = std::pair<cocos2d::Vec2, cocos2d::Vec2>;
+using segList_t = std::list<std::pair<cocos2d::Vec2, cocos2d::Vec2>>;
+using segListIterator_t = segList_t::iterator;
 
 class Polygon {
 
 public:
     Polygon();
 
-    const std::list<std::pair<cocos2d::Vec2, cocos2d::Vec2>> &GetSegments() const;
-
-    void AddSegment(std::pair<cocos2d::Vec2, cocos2d::Vec2> seg);
-
-    float CalcCutPoint(cocos2d::Vec2 a_start, cocos2d::Vec2 a_end, cocos2d::Vec2 b_start, cocos2d::Vec2 b_end);
-
-    void BreakSegment(segIterator it, float breakRatio);
-
-    void Crop(cocos2d::Vec2 pos, int dir);
-
-    int Crop(cocos2d::Vec2 start, cocos2d::Vec2 end);
-
-    segIterator Ray(cocos2d::Vec2 startPos, cocos2d::Vec2 dir);
-
     void Initial_Square(cocos2d::Vec2 leftBottom, int width);
 
     void Initial_Custom1(cocos2d::Vec2 leftBottom, int width);
 
+    const segList_t &GetSegments() const;
+
+    void AddSegment(seg_t seg);
+
+    void Crop(cocos2d::Vec2 pos, int dir, cocos2d::Vec2 ballPos);
+
+    bool IsPointInsidePolygon(cocos2d::Vec2 point) const;
+
+    Polygon &operator=(const Polygon &second);
+
 private:
-    std::list<std::pair<cocos2d::Vec2, cocos2d::Vec2>> _segments;
+    segList_t _segments;
+
+    segListIterator_t Ray(cocos2d::Vec2 startPos, cocos2d::Vec2 dir);
+
+    int RayCount(cocos2d::Vec2 startPos, cocos2d::Vec2 dir) const;
+
+    float CalcCutPoint(cocos2d::Vec2 a_start, cocos2d::Vec2 a_end, cocos2d::Vec2 b_start, cocos2d::Vec2 b_end) const;
+
+    cocos2d::Vec2 BreakSegment(segListIterator_t it, float breakRatio);
+
+    void SplitPolygon(cocos2d::Vec2 &pos, const segListIterator_t &it1, const segListIterator_t &it2,
+                      cocos2d::Vec2 &rayCollisionPoint1, cocos2d::Vec2 &rayCollisionPoint2, Polygon *&poly1,
+                      Polygon *&poly2) const;
 };
 
 
